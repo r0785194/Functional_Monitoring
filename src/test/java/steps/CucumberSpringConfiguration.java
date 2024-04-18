a572c803002778932aa67e0a930487699f86f7db
Laura Nijsen <r0785194@ucll.be>
2024-04-18 15:35:17 +0200

    added seperate cucumberconfigfile

--- src/test/java/steps/CucumberSpringConfiguration.java
+++ src/test/java/steps/CucumberSpringConfiguration.java
@@ -4,6 +4,7 @@ import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import refleqt.MonitoringApplication;
 
-
+@CucumberContextConfiguration
+@SpringBootTest(classes = MonitoringApplication.class)
 public class CucumberSpringConfiguration {
 }
