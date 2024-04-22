126f5d348774b51dc4ec7ec6f4b3f3e230b2df2b
Laura Nijsen <r0785194@ucll.be>
2024-04-22 16:21:42 +0200

    change config.yml

--- src/test/java/DriverConfig/WebDriverSingleton.java
+++ src/test/java/DriverConfig/WebDriverSingleton.java
@@ -2,13 +2,32 @@ package DriverConfig;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public abstract class WebDriverSingleton {
+    private static ChromeOptions options = new ChromeOptions();
 
+    public static ChromeOptions getOptions() {
+        options.addArguments("--no-sandbox");
+        return options;
+    }
     private static WebDriver driver;
     public static WebDriver getDriver() {
         if (driver == null) {
-            driver = new ChromeDriver();
+            if (isRunningOnCircleCI()){
+                try {
+                    driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), getOptions());
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            else {
+                driver = new ChromeDriver(getOptions());
+            }
         }
         return driver;
     }
@@ -18,5 +37,9 @@ public abstract class WebDriverSingleton {
             driver.quit();
         }
         return driver = null;
+    }
+
+    private static boolean isRunningOnCircleCI() {
+        return Boolean.parseBoolean(System.getenv("CI"));
     }
 }
