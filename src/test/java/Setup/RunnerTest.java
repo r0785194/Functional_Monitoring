1e2f1816e8dd876cd14bce2dd84c01e203c3f06d
Laura Nijsen <r0785194@ucll.be>
2024-04-23 10:23:59 +0200

    add CucumberSpringConfiguration.java for glue

--- src/test/java/Setup/RunnerTest.java
+++ src/test/java/Setup/RunnerTest.java
@@ -2,7 +2,6 @@ package Setup;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import refleqt.MonitoringApplication;
@@ -10,7 +9,6 @@ import refleqt.MonitoringApplication;
 @RunWith(Cucumber.class)
 @SpringBootTest(classes = { MonitoringApplication.class,
                     RunnerTest.class})
-@CucumberContextConfiguration
 @CucumberOptions(plugin = {"pretty", "json:target/cucumber-reports/json/report.json"},glue = ("steps"), features = "src/test/resources/features")
 public class RunnerTest {
 }
